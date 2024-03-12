3151c9f8dc8bfc64f06af4817d86d99897153901
maruf-ahmed <marufahamed1110@gmail.com>
2024-03-12 13:17:09 +0600

    handle add to card button

--- src/components/bottles/Bottle.jsx
+++ src/components/bottles/Bottle.jsx
@@ -1,13 +1,16 @@
-function Bottle({ bottle }) {
+function Bottle({ bottle, handleAddToCart }) {
   const { name, img, price } = bottle;
-  console.log(bottle);
+  //   console.log(bottle);
   return (
     <div>
       <div className=" bg-purple-100 m-5 p-4 rounded-xl border border-purple-500">
         <img className=" w-64" src={img} alt="" />
         <h2 className=" font-semibold text-lg text-gray-700 py-2">{name}</h2>
         <h2>Price : ${price}</h2>
-        <button className=" bg-purple-500 text-white py-2 px-4 rounded-lg mt-3 hover:bg-purple-400 duration-200">
+        <button
+          onClick={()=>handleAddToCart(bottle)}
+          className=" bg-purple-500 text-white py-2 px-4 rounded-lg mt-3 hover:bg-purple-400 duration-200"
+        >
           Purchese
         </button>
       </div>
--- src/components/bottles/Bottles.jsx
+++ src/components/bottles/Bottles.jsx
@@ -4,6 +4,8 @@ import Bottle from "./Bottle";
 
 function Bottles() {
   const [bottle, setBottle] = useState([]);
+  const [addToCart, setAddToCart] = useState([]);
+
   useEffect(() => {
     const fetchBottle = async () => {
       let res = await fetch("bottle.json");
@@ -12,11 +14,23 @@ function Bottles() {
     };
     fetchBottle();
   }, []);
+  const handleAddToCart = (bottle) => {
+    const newAddToCart = [...addToCart, bottle];
+    setAddToCart(newAddToCart);
+  };
+  console.log(addToCart.length);
   return (
     <div>
+      <h2 className=" text-center text-xl font-bold pt-2">
+        Card : {addToCart.length}
+      </h2>
       <div className="grid grid-cols-4 max-w-7xl mx-auto">
         {bottle.map((items, index) => (
-          <Bottle key={index} bottle={items} />
+          <Bottle
+            handleAddToCart={handleAddToCart}
+            key={index}
+            bottle={items}
+          />
         ))}
       </div>
     </div>
